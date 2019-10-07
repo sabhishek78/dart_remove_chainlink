7511ff96e3d1c646f8446659fb51467f556bebb5
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-10-07 12:37:10 +0530

    all nodes tested and removed successfully

--- bin/main.dart
+++ bin/main.dart
@@ -24,10 +24,12 @@ main() {
   Link sixthLink = Link('Parvati');
   fifthLink.next = sixthLink;
 
-  print(firstLink);
+  sixthLink.next=null;
+
+  printChain(firstLink);
   print('-------');
-  removeLink(firstLink, 'Makiko');
-  print(firstLink);
+  //removeLink(firstLink, 'Raj');
+  printChain(removeLink(firstLink, 'Parvati'));
 }
 
 // Printing the linkTree
@@ -36,11 +38,34 @@ void printChain(Link link) {
   while (currLink != null) {
     print(currLink.value);
     currLink = currLink.next;
-  }
+  };
 }
 
 // Exercise: Write a function which takes in a link and a word. It should remove the link with value matching word
-void removeLink(Link firstLink, String word) {
+Link removeLink(Link firstLink, String word) {
   // Implement function here
+ Link currLink=firstLink;
+ Link temp;
+ bool flagNodeFound=false;
+ while(currLink.next!=null)
+   {
+     if(currLink.value==word)
+       {
+         temp=currLink.next;
+         firstLink=temp;
+         flagNodeFound=true;
+         return firstLink;
+       }// code for deleting  the first node
+     else if(currLink.next.value==word)
+           {
+             temp=currLink.next.next;
+             currLink.next=temp;
+             flagNodeFound=true;
+             return firstLink;
+           }
+     currLink=currLink.next;//move to next Link
+     
+   }
 
-}
+     }
+
